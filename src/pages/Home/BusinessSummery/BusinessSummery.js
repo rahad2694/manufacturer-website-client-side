246ef0cd13d3d358ba1dd34cfd508eb281791a11
rahad2694 <rahad.haw@gmail.com>
2022-05-26 09:10:24 +0600

    Business Summery added

---
+++ src/pages/Home/BusinessSummery/BusinessSummery.js
@@ -0,0 +1,35 @@
+import { faMoneyBillTrendUp, faPeopleGroup, faTruckFast, faUsersGear } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const BusinessSummery = () => {
+    return (
+        <div className='mb-20 mt-5'>
+            <h1 className='text-3xl font-bold my-10 italic mx-auto text-gray-700 max-w-4xl'><span title='Allumin Apparatus' className='font-title text-5xl text-orange-500'>A-A</span> at a Glance:</h1>
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-5 gap-10 lg:pt-10'>
+                <div>
+                    <p className='text-8xl mb-3'><FontAwesomeIcon icon={faTruckFast} /></p>
+                    <p>30,000</p>
+                    <p>Deliveries/Year</p>
+                </div>
+                <div>
+                    <p className='text-8xl mb-3'><FontAwesomeIcon icon={faMoneyBillTrendUp} /></p>
+                    <p>$90,000</p>
+                    <p>Yearly Turnover</p>
+                </div>
+                <div>
+                    <p className='text-8xl mb-3'><FontAwesomeIcon icon={faUsersGear} /></p>
+                    <p>50,000</p>
+                    <p>Happy Customers</p>
+                </div>
+                <div>
+                    <p className='text-8xl mb-3'><FontAwesomeIcon icon={faPeopleGroup} /></p>
+                    <p>1,000</p>
+                    <p>Expert personnel's</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BusinessSummery;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import BusinessSummery from './BusinessSummery/BusinessSummery';
 import CarouselBanner from './CarouselBanner';
 import PurchaseSteps from './PurchaseSteps';
 import Reviews from './Reviews';
@@ -11,6 +12,7 @@ const Home = () => {
             <h1 className='text-3xl  mb-5 mt-10'>Welcome to <span className='font-title text-5xl text-orange-500'>Allumin Apparatus Ltd.</span></h1>
             <h1 className='text-3xl font-bold my-10 italic mx-auto text-gray-700 max-w-4xl'>Exclusive Products</h1>
             <Tools></Tools>
+            <BusinessSummery></BusinessSummery>
             <PurchaseSteps></PurchaseSteps>
             <Reviews></Reviews>
         </div>
